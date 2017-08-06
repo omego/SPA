8162d7f60452905076aef16ff7b950595c05eecf
omego <mserihi@gmail.com>
2017-08-06 11:26:21 +0300

    Init. Array

--- resources/views/initiative/show.blade.php
+++ resources/views/initiative/show.blade.php
@@ -87,5 +87,28 @@
                                                     </tr>
                                                 </tbody>
                                             </table>
+
+<table>
+        <thead>
+            <th>action_plan_title</th>
+            <th>actions</th>
+        </thead>
+        <tbody>
+            @foreach($action_plans as $action_plan) 
+            <tr>
+                <td>{!!$action_plan->action_plan_title!!}</td>
+                <td>
+                    <div class = 'row'>
+                        <a href = '#modal1' class = 'delete btn-floating modal-trigger red' data-link = "/action_plan/{!!$action_plan->id!!}/deleteMsg" ><i class = 'material-icons'>delete</i></a>
+                        <a href = '#' class = 'viewEdit btn-floating blue' data-link = '/action_plan/{!!$action_plan->id!!}/edit'><i class = 'material-icons'>edit</i></a>
+                        <a href = '#' class = 'viewShow btn-floating orange' data-link = '/action_plan/{!!$action_plan->id!!}'><i class = 'material-icons'>info</i></a>
+                    </div>
+                </td>
+            </tr>
+            @endforeach 
+        </tbody>
+    </table>
+    {!! $action_plans->render() !!}
+
                                         </div>
                                         @endsection
