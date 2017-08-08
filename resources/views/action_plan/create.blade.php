7287236a5c6caf8d70d9c653603b13bf8eff1ebd
omego <mserihi@gmail.com>
2017-08-08 08:25:51 +0300

    Upload in Action plan

--- resources/views/action_plan/create.blade.php
+++ resources/views/action_plan/create.blade.php
@@ -10,24 +10,13 @@
         <button class = 'btn blue'>action_plan Index</button>
     </form>
     <br>
-    <form method = 'POST' action = '{!!url("action_plan")!!}'>
+    <form enctype="multipart/form-data" method = 'POST' action = '{!!url("action_plan")!!}'>
         <input type = 'hidden' name = '_token' value = '{{ Session::token() }}'>
         <div class="input-field col s6">
             <input id="action_plan_title" name = "action_plan_title" type="text" class="validate">
             <label for="action_plan_title">action_plan_title</label>
         </div>
-        <div class="input-field col s6">
-            <input id="action_plan_updates" name = "action_plan_updates" type="text" class="validate">
-            <label for="action_plan_updates">action_plan_updates</label>
-        </div>
-        <div class="input-field col s6">
-            <input id="action_plan_risks" name = "action_plan_risks" type="text" class="validate">
-            <label for="action_plan_risks">action_plan_risks</label>
-        </div>
-        <div class="input-field col s6">
-            <input id="action_plan_resources" name = "action_plan_resources" type="text" class="validate">
-            <label for="action_plan_resources">action_plan_resources</label>
-        </div>
+
         <div class="input-field col s6">
             <input id="action_plan_start" name = "action_plan_start" type="text" class="validate">
             <label for="action_plan_start">action_plan_start</label>
@@ -36,10 +25,7 @@
             <input id="action_plan_end" name = "action_plan_end" type="text" class="validate">
             <label for="action_plan_end">action_plan_end</label>
         </div>
-        <div class="input-field col s6">
-            <input id="action_plan_approval" name = "action_plan_approval" type="text" class="validate">
-            <label for="action_plan_approval">action_plan_approval</label>
-        </div>
+
         <div class="input-field col s12">
             <select name = 'initiative_id'>
                 @foreach($initiatives as $key => $value) 
