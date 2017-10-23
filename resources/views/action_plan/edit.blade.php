2943d4c366f80252c429ef3117e79dbcc0432a6e
omego <mserihi@gmail.com>
2017-10-23 12:12:12 +0300

    Has role fix

--- resources/views/action_plan/edit.blade.php
+++ resources/views/action_plan/edit.blade.php
@@ -72,7 +72,7 @@
  </p>
  @endcan
  <br>
- @hasrole('Admin')
+ {{-- @hasrole('Admin') --}}
         <div class="input-field col s12">
             <select name = 'initiative_id'>
                 @foreach($initiatives as $key => $value)
@@ -85,8 +85,8 @@
             </select>
             <label>initiatives Select</label>
         </div>
-@endhasrole
-@can('edit initiatives')
+{{-- @endhasrole --}}
+{{-- @can('edit initiatives') --}}
         <div class="input-field col s12">
             <select name = 'user_id'>
                 @if (empty($action_plan->user_id))
@@ -103,7 +103,7 @@
             </select>
             <label>Assigned user</label>
         </div>
-@endcan
+
         <button class = 'btn red' type ='submit'>Update</button>
     </form>
 
