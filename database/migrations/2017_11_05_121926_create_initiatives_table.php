23059fd68ac31b20d5d6b14f922eb6fa3e6ed08f
Ssy3 <ssy.vip@gmail.com>
2017-11-05 15:20:08 +0300

    php artisan migrate:generate initiatives

---
+++ database/migrations/2017_11_05_121926_create_initiatives_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateInitiativesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('initiatives', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('initiative_title', 191);
+			$table->text('initiative_description');
+			$table->text('kpi_description', 65535)->nullable();
+			$table->string('kpi_previous', 11);
+			$table->date('kpi_previous_date')->nullable();
+			$table->string('kpi_current', 11);
+			$table->date('kpi_current_date')->nullable();
+			$table->string('kpi_target', 11);
+			$table->date('kpi_target_date')->nullable();
+			$table->string('status', 22)->nullable();
+			$table->text('why_if_not', 65535)->nullable();
+			$table->text('dod_note', 65535)->nullable();
+			$table->integer('project_id')->unsigned()->nullable()->index('initiatives_project_id_foreign');
+			$table->integer('user_id')->unsigned()->nullable()->index('user_id');
+			$table->timestamps();
+			$table->softDeletes();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('initiatives');
+	}
+
+}
